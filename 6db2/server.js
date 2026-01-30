b0e5886b731561e3b6ff63a989807035f2752f29
Ashish-Bhatti <60403355+Ashish-Bhatti@users.noreply.github.com>
2026-01-30 21:19:28 +0530

    update

--- 6db2/server.js
+++ 6db2/server.js
@@ -8,7 +8,7 @@ const app = require('./src/app')
 const mongoose = require('mongoose')
 
 function connectToDB(){
-    mongoose.connect("mongodb+srv://ashishbhattidev_db_user:CPUX7KRgfjM4EXlz@cluster0.dkmbyz9.mongodb.net/6db2")
+    mongoose.connect("")
     .then(()=>{
         console.log('connected to database')
     })

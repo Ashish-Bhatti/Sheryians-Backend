dc2b47e53219baa4509457c370b1e18884f9b9a1
Ashish-Bhatti <60403355+Ashish-Bhatti@users.noreply.github.com>
2026-04-12 21:09:25 +0530

    update

--- fullproject/src/controllers/post.controller.js
+++ fullproject/src/controllers/post.controller.js
@@ -21,7 +21,7 @@ async function createPostController(req, res) {
     let decoded = null;
 
     try {
-    decoded = jwt.verify(token, process.env.JWT_SECRET);
+        decoded = jwt.verify(token, process.env.JWT_SECRET);
     } catch (err) {
         return res.status(401).json({
             message: 'unauthorized access',
@@ -48,6 +48,80 @@ async function createPostController(req, res) {
     });
 }
 
+async function getPostController(req, res) {
+    const token = req.cookies.token;
+
+    if (!token) {
+        return res.status(401).json({
+            message: 'Unauthorized access',
+        });
+    }
+
+    let decoded;
+    try {
+        decoded = verify.jwt(token, pocess.env.JWT_SECRET);
+    } catch (err) {
+        return res.status(401).json({
+            message: 'token invalid',
+        });
+    }
+
+    const userId = decoded.id;
+
+    const posts = await postModel.find({ user: userId });
+
+    res.status(200).json({
+        message: 'post fetched successfully',
+        posts,
+    });
+}
+
+async function getPostDetailsContoller(req, res) {
+    const token = req.cookies.token;
+
+    if (!token) {
+        return res.status(401).json({
+            message: 'Unauthorized access',
+        });
+    }
+
+    let decoded;
+
+    try {
+        decoded = jwt.verify(token, JWT_SECRET);
+    } catch (error) {
+        return res.status(401).json({
+            message: 'invalid token',
+        });
+    }
+
+    const userId = decoded.id;
+    const postId = req.params.postId;
+
+    const post = await postModel.findById(postId);
+
+    if (!post) {
+        return res.status(404).json({
+            message: 'post not found',
+        });
+    }
+
+    const isValidUser = post.user.toString() === userId;
+
+    if (!isValidUser) {
+        return res.status(403).json({
+            message: 'forbidden content',
+        });
+    }
+
+    return res.status(200).json({
+        message: 'post fetched successfully',
+        post,
+    });
+}
+
 module.exports = {
     createPostController,
+    getPostController,
+    getPostDetailsContoller,
 };
--- fullproject/src/routes/post.routes.js
+++ fullproject/src/routes/post.routes.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const postRouter = express.Router();
 const postController = require('../controllers/post.controller');
-const multer  = require('multer') // middleware for handling multipart/form-data, which is primarily used for uploading files. In this case, we are using it to handle image uploads for posts.
-const upload = multer({storage: multer.memoryStorage()})
+const multer = require('multer'); // middleware for handling multipart/form-data, which is primarily used for uploading files. In this case, we are using it to handle image uploads for posts.
+const upload = multer({ storage: multer.memoryStorage() });
 
-postRouter.post('/', upload.single('image'), postController.createPostController)
+/**
+ * POST /api/posts [protected]
+ * - req.body = { caption,image-file }
+ */
+postRouter.post('/', upload.single('image'), postController.createPostController);
+
+/**
+ * GET /api/posts/ [protected]
+ */
+postRouter.get('/', postController.getPostController);
+
+/**
+ * GET /api/posts/details/:postid
+ * - return an detail about specific post with the id. also check whether the post belongs to the user that the request come from
+ */
+postRouter.get('/details/:postId',postController.getPostDetailsContoller)
 
 module.exports = postRouter;

aba40795e12d43e7ae18556e2eb3adbeebf64457
Ashish-Bhatti <60403355+Ashish-Bhatti@users.noreply.github.com>
2026-07-11 20:51:13 +0530

    update

--- Docker_Compose/Frontend/src/app/App.jsx
+++ Docker_Compose/Frontend/src/app/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
   },[])
   return (
     <div>
-    <h1>eeee</h1>
+    <h1>Users</h1>
       <ul>
         {users.map((user)=>{
           return <li key={user.id}>{user.name}</li>
